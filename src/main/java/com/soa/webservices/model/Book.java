9b04039a628290227ada6bc43790d6b2c73b4254
Dell <shayma.aloui02@gmail.com>
2025-11-30 02:35:30 +0100

    refactor: replace Lombok annotations with explicit getters and setters in model classes

--- src/main/java/com/soa/webservices/model/Book.java
+++ src/main/java/com/soa/webservices/model/Book.java
@@ -3,15 +3,9 @@ package com.soa.webservices.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "books")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Book {
     
     @Id
@@ -35,4 +29,41 @@ public class Book {
     
     @Column(name = "publication_year")
     private Integer publicationYear;
+    
+    // Constructeurs
+    public Book() {}
+    
+    public Book(Long id, String title, String author, String isbn, Double price, String description, Integer publicationYear) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+        this.description = description;
+        this.publicationYear = publicationYear;
+    }
+    
+    // Getters
+    public Long getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getIsbn() { return isbn; }
+    public Double getPrice() { return price; }
+    public String getDescription() { return description; }
+    public Integer getPublicationYear() { return publicationYear; }
+    
+    // Setters
+    public void setId(Long id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public void setPrice(Double price) { this.price = price; }
+    public void setDescription(String description) { this.description = description; }
+    public void setPublicationYear(Integer publicationYear) { this.publicationYear = publicationYear; }
+    
+    // toString, equals, hashCode
+    @Override
+    public String toString() {
+        return "Book{id=" + id + ", title='" + title + "', author='" + author + "', isbn='" + isbn + "'}";
+    }
 }
--- src/main/java/com/soa/webservices/model/WeatherResponse.java
+++ src/main/java/com/soa/webservices/model/WeatherResponse.java
@@ -1,12 +1,10 @@
 package com.soa.webservices.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
 
 /**
  * Modèle pour la réponse de l'API météo Open-Meteo
  */
-@Data
 public class WeatherResponse {
     
     private Double latitude;
@@ -28,7 +26,31 @@ public class WeatherResponse {
     @JsonProperty("current_weather")
     private CurrentWeather currentWeather;
     
-    @Data
+    // Getters & Setters
+    public Double getLatitude() { return latitude; }
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+    
+    public Double getLongitude() { return longitude; }
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
+    
+    public Double getGenerationtimeMs() { return generationtimeMs; }
+    public void setGenerationtimeMs(Double generationtimeMs) { this.generationtimeMs = generationtimeMs; }
+    
+    public Integer getUtcOffsetSeconds() { return utcOffsetSeconds; }
+    public void setUtcOffsetSeconds(Integer utcOffsetSeconds) { this.utcOffsetSeconds = utcOffsetSeconds; }
+    
+    public String getTimezone() { return timezone; }
+    public void setTimezone(String timezone) { this.timezone = timezone; }
+    
+    public String getTimezoneAbbreviation() { return timezoneAbbreviation; }
+    public void setTimezoneAbbreviation(String timezoneAbbreviation) { this.timezoneAbbreviation = timezoneAbbreviation; }
+    
+    public Double getElevation() { return elevation; }
+    public void setElevation(Double elevation) { this.elevation = elevation; }
+    
+    public CurrentWeather getCurrentWeather() { return currentWeather; }
+    public void setCurrentWeather(CurrentWeather currentWeather) { this.currentWeather = currentWeather; }
+    
     public static class CurrentWeather {
         private String time;
         private Integer interval;
@@ -40,5 +62,27 @@ public class WeatherResponse {
         private Integer isDay;
         
         private Integer weathercode;
+        
+        // Getters & Setters
+        public String getTime() { return time; }
+        public void setTime(String time) { this.time = time; }
+        
+        public Integer getInterval() { return interval; }
+        public void setInterval(Integer interval) { this.interval = interval; }
+        
+        public Double getTemperature() { return temperature; }
+        public void setTemperature(Double temperature) { this.temperature = temperature; }
+        
+        public Double getWindspeed() { return windspeed; }
+        public void setWindspeed(Double windspeed) { this.windspeed = windspeed; }
+        
+        public Integer getWinddirection() { return winddirection; }
+        public void setWinddirection(Integer winddirection) { this.winddirection = winddirection; }
+        
+        public Integer getIsDay() { return isDay; }
+        public void setIsDay(Integer isDay) { this.isDay = isDay; }
+        
+        public Integer getWeathercode() { return weathercode; }
+        public void setWeathercode(Integer weathercode) { this.weathercode = weathercode; }
     }
 }
